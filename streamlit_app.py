dd95defc9d9afbc833aa42ee8eefec44396ce9dc
Jpreets <Jpreet.singh20@gmail.com>
2023-07-20 20:20:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,12 +14,12 @@ def get_fruityvice_data(this_fruit_choice):
     return fruityvice_normalized;
 
 def get_fruit_load_list():
-    with  my_cnx.cursor() as my_cur
-    my_cur.execute("SELECT * from fruit_load_list ")
+    with  my_cnx.cursor() as my_cur;
+    my_cur.execute("SELECT * from fruit_load_list ");
     return my_cur.fetchall();
     
 def insert_row_snowflake(new_fruit):
-    with  my_cnx.cursor() as my_cur
+    with  my_cnx.cursor() as my_cur;
     my_cur.execute("insert into fruit_load_list values('"+new_fruit+"')");
     return 'Thanks for Adding new Fruit '+new_fruit;
 
